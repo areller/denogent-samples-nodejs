d73acad31d5ea15f2aef2598977e559c0dd7e68c
areller <areller.gm@gmail.com>
2020-10-07 16:51:37 -0400

    add dependency

--- build/build.ts
+++ build/build.ts
@@ -1,11 +1,11 @@
-import { createBuilder } from "https://deno.land/x/denogent/lib/core/builder.ts";
-import { task } from "https://deno.land/x/denogent/lib/core/task.ts";
-import nodejs from "https://deno.land/x/denogent/lib/build-kits/nodejs/nodejs.ts";
-import runtime from "https://deno.land/x/denogent/lib/runtime/runtime.ts";
-import { createGitHubActions } from "https://deno.land/x/denogent/lib/ci/gh-actions/gh-actions.ts";
-import docker from "https://deno.land/x/denogent/lib/docker/docker.ts";
-import git from "https://deno.land/x/denogent/lib/git/git.ts";
-import type { DockerRegistryCredentials } from "https://deno.land/x/denogent/lib/docker/args.ts";
+import { createBuilder } from "https://deno.land/x/denogent@v0.1.5/lib/core/builder.ts";
+import { task } from "https://deno.land/x/denogent@v0.1.5/lib/core/task.ts";
+import nodejs from "https://deno.land/x/denogent@v0.1.5/lib/build-kits/nodejs/nodejs.ts";
+import runtime from "https://deno.land/x/denogent@v0.1.5/lib/runtime/runtime.ts";
+import { createGitHubActions } from "https://deno.land/x/denogent@v0.1.5/lib/ci/gh-actions/gh-actions.ts";
+import docker from "https://deno.land/x/denogent@v0.1.5/lib/docker/docker.ts";
+import git from "https://deno.land/x/denogent@v0.1.5/lib/git/git.ts";
+import type { DockerRegistryCredentials } from "https://deno.land/x/denogent@v0.1.5/lib/docker/args.ts";
 
 const nodeDependency = nodejs.setup('latest');
 
@@ -17,6 +17,7 @@ const install = task('install')
 
 const test = task('test')
     .dependsOn(install)
+    .dependsOn(nodeDependency)
     .dependsOn(docker.service({ name: 'redis', image: 'redis', ports: [6379] }))
     .does(async ctx => {
         await runtime.command({ cmd: ['npm', 'test'], logger: ctx?.logger });
