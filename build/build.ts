e6b981ef38aceaa6cd020dec683d707902653859
areller <areller.gm@gmail.com>
2020-10-07 01:46:13 -0400

    use latest version

--- build/build.ts
+++ build/build.ts
@@ -3,9 +3,9 @@ import { task } from "https://deno.land/x/denogent/lib/core/task.ts";
 import nodejs from "https://deno.land/x/denogent/lib/build-kits/nodejs/nodejs.ts";
 import runtime from "https://deno.land/x/denogent/lib/runtime/runtime.ts";
 import { createGitHubActions } from "https://deno.land/x/denogent/lib/ci/gh-actions/gh-actions.ts";
-import docker from "https://deno.land/x/denogent@v0.1.4/lib/docker/docker.ts";
-import git from "https://deno.land/x/denogent@v0.1.4/lib/git/git.ts";
-import type { DockerRegistryCredentials } from "https://deno.land/x/denogent@v0.1.4/lib/docker/args.ts";
+import docker from "https://deno.land/x/denogent/lib/docker/docker.ts";
+import git from "https://deno.land/x/denogent/lib/git/git.ts";
+import type { DockerRegistryCredentials } from "https://deno.land/x/denogent/lib/docker/args.ts";
 
 const nodeDependency = nodejs.setup('latest');
 
