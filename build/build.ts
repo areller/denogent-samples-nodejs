d24bd138487788cb96186313590c849c67ccf28d
areller <areller.gm@gmail.com>
2020-10-13 00:01:17 -0400

    upgrade denogent

--- build/build.ts
+++ build/build.ts
@@ -1,11 +1,6 @@
-import { createBuilder } from "https://deno.land/x/denogent@v0.1.5/lib/core/builder.ts";
-import { task } from "https://deno.land/x/denogent@v0.1.5/lib/core/task.ts";
-import nodejs from "https://deno.land/x/denogent@v0.1.5/lib/build-kits/nodejs/nodejs.ts";
-import runtime from "https://deno.land/x/denogent@v0.1.5/lib/runtime/runtime.ts";
-import { createGitHubActions } from "https://deno.land/x/denogent@v0.1.5/lib/ci/gh-actions/gh-actions.ts";
-import docker from "https://deno.land/x/denogent@v0.1.5/lib/docker/docker.ts";
-import git from "https://deno.land/x/denogent@v0.1.5/lib/git/git.ts";
-import type { DockerRegistryCredentials } from "https://deno.land/x/denogent@v0.1.5/lib/docker/args.ts";
+import { createBuilder, docker, DockerRegistryCredentials, git, runtime, task } from "https://deno.land/x/denogent@v0.1.7/lib/mod.ts";
+import { createGitHubActions } from "https://deno.land/x/denogent@v0.1.7/lib/ci/gh-actions/mod.ts";
+import { nodejs } from "https://deno.land/x/denogent@v0.1.7/lib/build-kits/nodejs/mod.ts";
 
 const nodeDependency = nodejs.setup('latest');
 
